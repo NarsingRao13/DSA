b1d96f45ba9eb2506922c607d10d3a36e308dc48
Narsing Rao Karukola <narsingrao.k@techouts.com>
2022-02-09 23:28:09 +0530

    Initial Commit

---
+++ src/com/resursion/Factorial.java
@@ -0,0 +1,17 @@
+package com.resursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+	
+	public static int fact(int n) {
+		if(n<=1)
+			return 1;
+		return (n*fact(n-1));
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(fact(sc.nextInt()));
+	}
+}
---
+++ src/com/resursion/FibonacciSeries.java
@@ -0,0 +1,21 @@
+package com.resursion;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+	
+	public static int fib(int n) {
+		if(n<=0) {
+			return 0;
+		} else if(n == 1 || n== 2)
+			return n-1;
+		else {
+			return fib(n-1)+fib(n-2);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(fib(sc.nextInt()));
+	}
+}
